2b0ac6497cd8fcf95a924e9275ad3de61f1658b3
Trplnr <87922281+Trioplane@users.noreply.github.com>
2025-04-06 13:55:10 +0800

    optimization on build file, singular building, new example

--- src/screen_effects/buildScreenEffects.js
+++ src/screen_effects/buildScreenEffects.js
@@ -4,16 +4,22 @@ import { ensureDir } from "https://deno.land/std@0.181.0/fs/ensure_dir.ts";
 // Replace with the namespace you use!
 const NAMESPACE = "example";
 
+// This logs what the builder is currently doing.
+// If compiling is taking a while, disabling this will improve compilation time.
+const debugMode = false;
+
 // If you want the font JSON files to be formatted, make this boolean true.
 const prettyPrintFontFiles = false;
 
 // If you want to see the ffmpeg logs, make this boolean true.
+// If compiling is taking a while, disabling this will improve compilation time.
 const showFFMPEGLogs = false;
 // -----------------------------------
 
 
 // Thanks for using my library!! - @trplnr
 
+const fileToBuild = Deno.args[0]
 
 // DO NOT TOUCH! ⚠
 const PATHS = {
@@ -25,13 +31,60 @@ const PATHS = {
 ensureDir(PATHS.font_files)
 ensureDir(PATHS.textures)
 
-console.log(`🟡 Compiling screen effects.`)
-
-for await (const file of Deno.readDir(PATHS.screen_effects)) {
-    if (!file.isFile) continue
-    if (!file.name.endsWith(".png")) continue
+function writeFontAndImageFiles(frameCount, width, imagePath, fileName) {
+    const baseName = fileName.replace(".png","")
+    const baseOutputImagePath = `${PATHS.textures}/${baseName}`
+    const baseOutputFontFilePath = `${PATHS.font_files}/${baseName}`
+    const baseImageResourcePath = `${NAMESPACE}:font/scrfx/${baseName}`
+    for (let i = 0; i < frameCount; i++) {
+        if (debugMode) console.log(`----> 🟡 Writing frame '${baseName}${i}'`)
+        const outputImagePath = `${baseOutputImagePath}${i}.png`
+        const outputFontFilePath = `${baseOutputFontFilePath}${i}.json`
+        // writing font file json
+        if (debugMode) console.log(`------> 🟡 Writing font file '${outputFontFilePath}'`)
+        const imageResourcePath = `${baseImageResourcePath}${i}.png`
+        const fontFileContents = JSON.stringify({
+                providers: [
+                    {
+                        type: "bitmap",
+                        file: imageResourcePath,
+                        height: width,
+                        ascent: -2000,
+                        chars: [
+                          "\uE000"
+                        ]
+                }
+            ]
+        }, null, prettyPrintFontFiles ? 4 : 0)
     
-    const imagePath = `${PATHS.screen_effects}${file.name}`
+        Deno.writeFile(outputFontFilePath, new TextEncoder().encode(fontFileContents))
+        if (debugMode) console.log(`------> ✅ Successfully wrote font file '${outputFontFilePath}'`)
+        
+        // ffmpeg to split the image into individual frames
+        if (debugMode) console.log(`------> 🟡 Writing image '${imagePath}'`)
+        const cropCommand = new Deno.Command("ffmpeg", {
+            args: [
+                `-y`,
+                `-i`, imagePath,
+                `-vf`, `crop=w=${width}:h=${width}:x=0:y=${width * i}`,
+                `-frames:v`, `1`,
+                `-update`, `true`,
+                outputImagePath
+            ],
+            stdout: showFFMPEGLogs ? "inherit" : "null",
+            stderr: showFFMPEGLogs ? "inherit" : "null",
+        })
+        cropCommand.output()
+        if (debugMode) console.log(`------> ✅ Successfully wrote image '${outputImagePath}'`)
+        
+        if (debugMode) console.log(`----> ✅ Successfully wrote frame '${baseName}${i}'`)
+    }
+}
+
+async function buildScreenEffect(file) {
+    if (!file.endsWith(".png")) throw new TypeError("Screen effect spritesheet must be a .png!")
+
+    const imagePath = `${PATHS.screen_effects}${file}`
     const image = await Deno.readFile(imagePath)
     const dataView = new DataView(image.buffer)
 
@@ -40,53 +93,37 @@ for await (const file of Deno.readDir(PATHS.screen_effects)) {
 
     const frameCount = height / width;
 
-    for (let i = 0; i < frameCount; i++) {
-        console.log(`----> 🟡 Writing frame '${file.name.replace(".png","")}${i}'`)
-        const outputImagePath = `${PATHS.textures}/${file.name.replace(".png","")}${i}.png`
-        const outputFontFilePath = `${PATHS.font_files}/${file.name.replace(".png","")}${i}.json`
-        
-        // writing font file json
-        console.log(`------> 🟡 Writing font file '${outputFontFilePath}'`)
-        const imageResourcePath = `${NAMESPACE}:font/scrfx/${file.name.replace(".png","")}${i}.png`
-        const fontFileContents = JSON.stringify({
-                providers: [
-                    {
-                        type: "bitmap",
-                        file: imageResourcePath,
-                    height: width,
-                    ascent: -2000,
-                    chars: [
-                      "\uE000"
-                    ]
-                }
-            ]
-        }, null, prettyPrintFontFiles ? 4 : 0)
-    
-        Deno.writeFile(outputFontFilePath, new TextEncoder().encode(fontFileContents))
-        console.log(`------> ✅ Successfully wrote font file '${outputFontFilePath}'`)
-        
-        // ffmpeg to split the image into individual frames
-        console.log(`------> 🟡 Writing image '${imagePath}'`)
-        const cropCommand = new Deno.Command("ffmpeg", {
-            args: [
-                    `-y`,
-                    `-i`, imagePath,
-                    `-vf`, `crop=w=${width}:h=${width}:x=0:y=${width * i}`,
-                    `-frames:v`, `1`,
-                    `-update`, `true`,
-                    outputImagePath
-                ]
-            })
-        const { stdout, stderr } = await cropCommand.output()
-        if (showFFMPEGLogs) {
-            console.log(new TextDecoder().decode(stdout))
-            console.log(new TextDecoder().decode(stderr))
-        }
-        console.log(`------> ✅ Successfully wrote image '${outputImagePath}'`)
-        
-        console.log(`----> ✅ Successfully wrote frame '${file.name.replace(".png","")}${i}'`)
-    }
-    console.log(`--> ✅ Successfully built ${imagePath}`)
+    writeFontAndImageFiles(frameCount, width, imagePath, file)
+
+    if (debugMode) console.log(`--> ✅ Successfully built ${imagePath}`)
 }
 
-console.log(`✅ Successfully compiled all screen effects.`)
+async function compileAllScreenEffects() {
+    for await (const file of Deno.readDir(PATHS.screen_effects)) {
+        if (!file.isFile) continue
+        if (!file.name.endsWith(".png")) continue
+
+        const imagePath = `${PATHS.screen_effects}${file.name}`
+        const image = await Deno.readFile(imagePath)
+        const dataView = new DataView(image.buffer)
+
+        const width = dataView.getUint32(16)
+        const height = dataView.getUint32(20)
+
+        const frameCount = height / width;
+
+        writeFontAndImageFiles(frameCount, width, imagePath, file.name)
+
+        if (debugMode) console.log(`--> ✅ Successfully built ${imagePath}`)
+    }
+}
+
+console.log(`🟡 Compiling screen effects.`)
+const startTime = performance.now();
+if (!fileToBuild) {
+    await compileAllScreenEffects();
+} else {
+    await buildScreenEffect(fileToBuild)
+}
+const endTime = performance.now() - startTime;
+console.log(`✅ Successfully compiled all screen effects! Took ${endTime / 1000} seconds.`)
